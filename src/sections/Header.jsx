b7c724f7315ca6971bfaa62e4cd5aee9b2da5165
Luis Murrugarra <luismurrugarra17@gmail.com>
2023-11-23 22:55:21 -0500

    feat: Hero and Header Components Responsive

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -2,7 +2,7 @@ export const Header = ({logoUrl}) => {
   const menuItems = ['Inicio', 'Películas', 'Géneros', 'Series']
   const listItemsMenu = () => {
     return (
-      <ul className='flex text-white justify-center items-center gap-6'>
+      <ul className='hidden lg:flex text-white justify-center items-center gap-6'>
         {
           menuItems.map((item) =>
             (
@@ -14,16 +14,20 @@ export const Header = ({logoUrl}) => {
     )
   }
   return (
-    <header className='w-full absolute z-10 py-6 flex justify-between px-12'>
-      <div className='flex justify-center items-center gap-14'>
-        <div className=''>
-          <img src={logoUrl} alt="cuevana logo"/>
+    <header className='w-full absolute z-10 py-6 px-12'>
+      <div className=' w-full flex justify-center'>
+        <div className='w-full flex justify-between max-w-6xl'>
+          <div className='flex justify-center items-center gap-14'>
+            <div className=''>
+              <img src={logoUrl} alt="cuevana logo" className='min-w-min'/>
+            </div>
+            {listItemsMenu()}
+          </div>
+          <div className='hidden md:flex justify-center items-center w-60'>
+            <input className='bg-gray-800 px-4 py-2 rounded-3xl text-gray-500 w-full' type="search"
+                   placeholder='Buscador...'/>
+          </div>
         </div>
-        {listItemsMenu()}
-      </div>
-      <div className='flex justify-center items-center w-60'>
-        <input className='bg-gray-800 px-4 py-2 rounded-3xl text-gray-500 w-full' type="search"
-               placeholder='Buscador...'/>
       </div>
     </header>
   )
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,9 @@
 export const Hero = ({title, overview, imageUrl}) => {
   return (
-    <div className='w-full relative pt-52 pb-36 md:pb-48'>
+    <div className='w-full relative pt-36 md:pt-44 lg:pt-52 pb-36 md:pb-48'>
       <img src={imageUrl} alt="logo" className='absolute h-full top-0 left-0 w-full opacity-20'/>
-      <div className='relative top-0 left-0 w-full px-12'>
-        <div className='flex flex-col gap-5'>
+      <div className='relative top-0 left-0 w-full px-12 flex justify-center'>
+        <div className='flex flex-col gap-5 max-w-6xl'>
           <div className="flex justify-start items-center gap-2">
             <h2 className='text-6xl text-white font-bold'>{title}</h2>
             <span className='mt-6 bg-amber-400 text-black font-bold text-2xs rounded-3xl py-0.5 px-1.5'>Película</span>
@@ -13,9 +13,9 @@ export const Hero = ({title, overview, imageUrl}) => {
             <p>1h 45min</p>
             <p>2023</p>
           </div>
-          <p className='text-gray-400 text-xl'>{overview}</p>
+          <p className='text-gray-400 text-md lg:text-xl'>{overview}</p>
           <div className="actions w-full h-full">
-            <a href="#" className='bg-blue-600 text-white text-xl px-4 py-2 rounded-3xl inline-block'>Ver Película</a>
+            <a href="#" className='bg-blue-600 text-white text-sm md:text-md lg:text-xl px-4 py-2 rounded-3xl inline-block'>Ver Película</a>
           </div>
         </div>
       </div>
